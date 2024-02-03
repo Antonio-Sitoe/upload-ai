0be46ac74ceab4a21d57f7b1837e8fe1e2b19cda
Antonio Sitoe <antoniositoehl@gmail.com>
2024-02-03 16:15:38 +0200

    ajustes

--- server/src/routes/get-all-prompts.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from 'fastify';
-import { prisma } from '../lib/prisma';
-
-export async function get_all_prompts(app: FastifyInstance) {
-  app.get('/prompts', async () => {
-    const prompts = await prisma.prompt.findMany();
-    return prompts;
-  });
-}
